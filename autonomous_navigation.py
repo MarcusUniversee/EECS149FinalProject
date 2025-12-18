5f8f43247646f87e5d5ac9e42184ed9162e6957e
JinMizar <asg2rg@virginia.edu>
2025-12-18 03:54:10 -0800

    Real life data finished

--- autonomous_navigation.py
+++ autonomous_navigation.py
@@ -42,9 +42,10 @@ TURN_SPEED = 0.35  # Motor speed for turning (0.0 to 1.0)
 MIN_TURN_SPEED = 0.25  # Minimum speed to ensure movement
 CONTROL_RATE = 50  # Hz - control loop update rate
 
-MAX_VEL = 0.5
+MAX_VEL = 0.3 # need to change
 MAX_ROT = 0.5
-CRUISING_SPEED = 0.5
+CRUISING_SPEED = 0.3 # need to change
+
 DIST_KP = 0.9
 DIST_KI = 0.0 
 DIST_KD = 0.01
@@ -62,7 +63,7 @@ STATE_ARRIVED = 2
 STATE_FINISHED = 3
 BOX_SIZE  = 30
 
-MIN_LIN_SPEED = 0.2
+MIN_LIN_SPEED = 0.2 # need to change
 MIN_ROT_SPEED = 0.02
 # =======================================================
 
